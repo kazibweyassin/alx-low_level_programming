78e80b4b76ac1e6ef53b005b18e3541d71696719
kazibweyassin <kazibweusama@gmail.com>
2023-02-17 21:45:26 -0800

    done

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -3,29 +3,29 @@
 #include <stdio.h>
 
 /**
- * main -entry point 
+ * main - entry point
  *
  * Return:always return 0
  *
  */
 int main(void)
-		{
-		int n;
+{
+	int n;
 
-		srand(time(0));
-		n = rand()- RAND_MAX/2;
-		if (n<0)
-		{
-		print ("%d is negative\n" n);
-		} else if (n == 0 )
-		{
-		printf("%dis zero\n", n)
-		} else
-		{
-		print("%d s positive\n" , n);
-		}
-		return (0);
-		}
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
+}
 
 
 
