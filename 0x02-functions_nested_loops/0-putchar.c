9dca820ce99e8dd254dfcdb7d18cdac2a5d9d00e
kazibweyassin <kazibweusama@gmail.com>
2023-02-22 02:48:49 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
-/**
- * main - Entry point
+/* 
+main - Entry point
  *
  * Return: Always returns 0
  *
  */
 int main(void)
+
 {
 	_putchar('_');
 	_putchar('p');
@@ -18,4 +18,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
+
 }
