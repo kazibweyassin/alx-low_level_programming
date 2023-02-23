fb882ef78552072b15be08931ccca0cbd2899f47
kazibweyassin <kazibweusama@gmail.com>
2023-02-23 15:09:55 -0800

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 !== 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			if (i % 3 == 0)
 			{
@@ -31,4 +31,3 @@ int main(void)
 	printf("\n");
 	return (0);
 }
-
