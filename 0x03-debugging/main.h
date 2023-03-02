325166665556454a3def41a81f2eb6775d0f52e8
kazibweyassin <kazibweusama@gmail.com>
2023-03-02 07:10:43 -0800

    header_file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
-#define MAIN.H
+#define MAIN_H
 
-#include <stdio.>h
+#include <stdio.h>
 
 void positve_or_negative(int i);
 void largest_numbers(int a, int b, int c);
-void print_remaining_days(int month, int day, year);
+void print_remaining_days(int month, int day, int year);
 void print_times_table(int n);
 #endif
