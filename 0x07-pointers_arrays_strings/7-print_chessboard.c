e9d0256f6ffcb41f383f6099955450f579a3dd0a
kazibweyassin <kazibweusama@gmail.com>
2023-03-06 12:21:51 -0800

    corrected some errors

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * print_chessboard - print in the terminal the chessboard
+ * @a: The character to print
+ *
+ */
+void print_chessboard(char(*a)[8])
+{
+	int b;
+	int c;
 
-void print_chessboard(char (*a)[8]) {
-    int i, j;
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            printf("%c ", a[i][j]);
-        }
-        printf("\n");
-    }
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
+		_putchar('\n');
+	}
 }
 
