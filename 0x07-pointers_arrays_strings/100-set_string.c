106ba36ffdfec7ae3874dd1bc0f977a9ea80feca
kazibweyassin <kazibweusama@gmail.com>
2023-03-06 12:40:39 -0800

    done

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,18 +1,11 @@
-#include "main.h"
-#include <stdio.h>
-
+#include "holberton.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * set_string -  sets a string
+ * @s: string to set
+ * @to: value to set to
  */
-int main(void)
+void set_string(char **s, char *to)
 {
-    char *s0 = "Bob Dylan";
-    char *s1 = "Robert Allen";
-
-    printf("%s, %s\n", s0, s1);
-    set_string(&s1, s0);
-    printf("%s, %s\n", s0, s1);
-    return (0);
+	*s = to;
 }
+
