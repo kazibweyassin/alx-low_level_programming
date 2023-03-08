2e764c0e8829f4d4ecc080f941e02be92e25e67e
kazibweyassin <kazibweusama@gmail.com>
2023-03-08 12:36:42 -0800

    a function that prints a string, followed by a new line.

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <unistd.h>
-int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -11,4 +10,6 @@ int _sqrt_recursion(int n);
 int _pow_recursion(int x, int y);
 int factorial(int n);
 int is_prime_number(int n);
+int _putchar(char c);
+
 #endif
