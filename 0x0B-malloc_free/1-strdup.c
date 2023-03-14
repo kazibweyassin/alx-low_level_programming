8aac8d1396d3ff31995e873036a25948704f8d1c
kazibweyassin <kazibweusama@gmail.com>
2023-03-14 12:40:17 -0700

    function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,21 +1,7 @@
-include "main.h"
-/**
- * _strlen - give the length of a string
- * @s: the string
- *
- * Return: the length of a string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0 ; s[i] != '\0' ; i++)
-		;
-	return (i);
-}
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * _strdup - returns a pointer which contains a copy of the string
  * given as a parameter
@@ -27,27 +13,27 @@ int _strlen(char *s)
  */
 char *_strdup(char *str)
 {
-	int i;
-	char *copy;
+        int i;
+        char *copy;
 
-	if (str == '\0')
-	{
-		return ('\0');
-	}
-	i = _strlen(str);
-	copy = malloc(sizeof(char) * i + 1);
-	if (copy == '\0')
-	{
-		return ('\0');
-	}
-	else
-	{
-		for (i = 0 ; str[i] != '\0' ; i++)
-		{
-			copy[i] = str[i];
-		}
-	}
-	copy[i] = '\0';
-	return (copy);
+        if (str == NULL)
+        {
+                return NULL;
+        }
+        i = strlen(str);
+        copy = malloc(sizeof(char) * (i + 1));
+        if (copy == NULL)
+        {
+                return NULL;
+        }
+        else
+        {
+                for (i = 0 ; str[i] != '\0' ; i++)
+                {
+                        copy[i] = str[i];
+                }
+        }
+        copy[i] = '\0';
+        return copy;
 }
 
