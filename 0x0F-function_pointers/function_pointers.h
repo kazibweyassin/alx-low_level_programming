dcfffa7c7bfea4fba4ce31ec63b07d3a909be505
kazibweyassin <kazibweusama@gmail.com>
2023-03-22 13:33:31 -0700

    function that executes a function given as a parameter on each element of an array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,22 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unistd.h>
-
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+void array_iterator(int *array, size_t size, void(*action)(int));
 int _putchar(char c);
-int (*get_op_func(char *s))(int, int);
-#endif 
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
