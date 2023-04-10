94d0d7837abc21bcc46da2a511d55d31c0a92c21
kazibweyassin <kazibweusama@gmail.com>
2023-04-10 12:50:22 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,10 +29,10 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	}
 
-	for (j = 0 ; text_content [j] != '\0'; j++)
+	for (j = 0 ; text_content[j] != '\0'; j++)
 		;
 
-	written_bytes = write(file_des, text_content,j);
+	written_bytes = write(file_des, text_content, j);
 
 	if (written_bytes == -1)
 	{
