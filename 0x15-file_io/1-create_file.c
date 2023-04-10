5ba7d15a7bb189584bc825c11a8637464a850796
kazibweyassin <kazibweusama@gmail.com>
2023-04-10 12:44:58 -0700

    a function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content: string to write to the file
+ *
+ * Return: 1 on Success or -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_des, j, written_bytes;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	file_des = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file_des == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
+	for (j = 0 ; text_content [j] != '\0'; j++)
+		;
+
+	written_bytes = write(file_des, text_content,j);
+
+	if (written_bytes == -1)
+	{
+		return (-1);
+	}
+
+	close(file_des);
+	return (1);
+}
