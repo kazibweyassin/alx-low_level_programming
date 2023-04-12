ada80f20b3fc1f3b3dd0a72a873c70c33a332eae
kazibweyassin <kazibweusama@gmail.com>
2023-04-12 07:28:36 -0700

    a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,50 +1,55 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - This file will copies the content of a file to another file
- * @argv: The argument vector
- * @argc: the argument count
- * Return: wil return 0 on Success
+ * main - This is the proram program to copy
+ * @ac: The argument count
+ * @av: The array of arguments
+ * Return: This will return a value
  */
-int main(int argc, char *argv[])
+int main(int ac, char **av)
 {
-	int fo, fu, reading, writing;
-	char buffer[1024];
+	int fdFrum, fd, just_wrote, reading;
+	char buff[1024];
 
-	if (argc != 3)
-	{dprintf(STDERR_FILENO, "Usage: copy file_from file_to\n");
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	fo = open(argv[1], O_RDONLY);
-	if (fo == -1)
+	fdFrum = open(av[1], O_RDONLY);
+	if (fdFrum == -1)
 	{
-		dprintf(STDERR_FILENO, "Fatal Error: I Can't read from file %s\n", argv[1]);
+		perror(STDERR_FILENO, "Err: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
-	fu = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fu == -1)
-	{dprintf(STDERR_FILENO, "Err:I  Can't write to %s\n", argv[2]), exit(99);
-	}
-
-	while ((reading = read(fo, buffer, 1024)) != 0)
+	fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd == -1)
 	{
-		if (reading == -1)
-		{dprintf(STDERR_FILENO, "Fatal Err: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		writing = write(fu, buffer, reading);
-		if (writing == -1)
-		{dprintf(STDERR_FILENO, " Err: Can't write to %s\n", argv[2]), exit(99);
+		perror(STDERR_FILENO, "Err: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	while ((reading = read(fdFrum, buff, 1024)) > 0)
+	{
+		just_wrote = write(fd, buff, reading);
+		if (just_wrote == -1)
+		{
+			dprintf(STDERR_FILENO, "Er: Can't write to %s\n", av[2]);
+			exit(99);
 		}
 	}
-
-	if (close(fo) == -1)
-	{dprintf(STDERR_FILENO, "Fatl Error: Can't close fd %d\n", fo), exit(100);
+	if (reading == -1)
+	{
+		perror(STDERR_FILENO, "Er: Can't read from file %s\n", av[1]);
+		exit(98);
 	}
-	if (close(fu) == -1)
-	{dprintf(STDERR_FILENO, "Fatal Error: Can't close fd %d\n", fu), exit(100);
+	if (close(fdFrum) == -1)
+	{
+		perror(STDERR_FILENO, "Er: Can't close fd %d", fdFrum);
+		exit(100);
+	}
+	if (close(fd) == -1)
+	{
+		perror(STDERR_FILENO, "Er: Can't close fd %d", fd);
+		exit(100);
 	}
 	return (0);
 }
